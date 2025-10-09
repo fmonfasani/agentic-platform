ce70dc64972a9c1125f1c8867ea0196d5ad87a62
Federico Monfasani <67231410+fmonfasani@users.noreply.github.com>
2025-10-08 23:06:05 -0300

    feat: add dashboard areas module and tests

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -1,7 +1,8 @@
 import { Module } from '@nestjs/common'
 import { AgentsModule } from './agents/agents.module'
+import { DashboardModule } from './dashboard/dashboard.module'
 
 @Module({
-  imports: [AgentsModule]
+  imports: [AgentsModule, DashboardModule]
 })
 export class AppModule {}
